59797271f5e8bfa02c480d45912ceddc25e47f3d
Terry Hearst <thearst3rd@gmail.com>
2022-08-01 23:40:17 -0400

    Added ramps to level, added restart button, other misc improvements

--- src/objects/marble.gd
+++ src/objects/marble.gd
@@ -3,21 +3,23 @@ extends RigidDynamicBody3D
 
 const MAGNITUDE = 20.0
 const TORQUE_MAGNITUDE = 5.0
-const LOOK_SENS = Vector2(0.005, 0.005)
+const LOOK_SENS = Vector2(0.0025, 0.0015)
 
-var look_direction := Vector3()
-var look_direction_raw := look_direction
+@onready var center_node: Node3D = $CenterNode
+@onready var spring_arm: SpringArm3D = $CenterNode/SpringArm3D
 
-@onready var spring_arm: SpringArm3D = $SpringArm3D
+@onready var look_direction := spring_arm.rotation
+@onready var look_direction_raw := look_direction
 
 
 func _ready() -> void:
-	spring_arm.top_level = true
+	center_node.top_level = true
+	center_node.position = position
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _process(_delta: float) -> void:
-	spring_arm.position = position
+	center_node.position = position
 	look_direction = look_direction_raw
 	look_direction.y = wrapf(look_direction.y, 0, TAU)
 	look_direction.x = clampf(look_direction.x, -PI/2 + 0.1, PI/2)
@@ -35,15 +37,25 @@ func _input(event: InputEvent) -> void:
 		var mouse_event := event as InputEventMouseButton
 		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		if event.is_action_pressed("restart"):
+			get_tree().reload_current_scene()
 
 
 func _physics_process(_delta: float) -> void:
-	var wishdir := Input.get_vector("left", "right", "forward", "backward")
+	# Don't normalize this vector to enable diagonal movement
+	var wishdir := Vector2(Input.get_axis("left", "right"), Input.get_axis("forward", "backward"))
 
 	var force_vector := (Vector3(wishdir.x, 0, wishdir.y) * MAGNITUDE).rotated(Vector3.UP, look_direction.y)
 	apply_central_force(force_vector)
 	var torque_vector := (Vector3(wishdir.y, 0, -wishdir.x) * TORQUE_MAGNITUDE).rotated(Vector3.UP, look_direction.y)
 	apply_torque(torque_vector)
 
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_pressed("jump") and is_on_floor():
 		apply_impulse(Vector3.UP * 10)
+
+
+# This doesn't work...
+# TODO: Replace this with something functional
+func is_on_floor() -> bool:
+	return get_colliding_bodies().size() > 0
