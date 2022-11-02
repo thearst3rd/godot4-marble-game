48439e29b8094558d661a64614e8ef4232016e2c
Terry Hearst <thearst3rd@gmail.com>
2022-11-01 20:46:21 -0400

    Use level names in level select

--- src/states/menu/level_select_menu.gd
+++ src/states/menu/level_select_menu.gd
@@ -13,8 +13,17 @@ const LEVELS = [
 
 func _ready() -> void:
 	for level_path in LEVELS:
+		var level_scene := load(level_path) as PackedScene
+		var state := level_scene.get_state()
+		var level_name = null
+		for prop_index in state.get_node_property_count(0):
+			if state.get_node_property_name(0, prop_index) == "level_name":
+				level_name = state.get_node_property_value(0, prop_index)
+				break
+		if level_name == null:
+			level_name = level_path
 		var button := Button.new()
-		button.text = level_path
+		button.text = level_name
 		button.connect("pressed", self.play_level.bind(level_path))
 		level_buttons.add_child(button)
 
