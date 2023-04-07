074c2325c2e70306149025c6b216101db4b81bf7
Terry Hearst <thearst3rd@gmail.com>
2023-04-06 22:37:33 -0400

    Update to Godot 4.0.2.stable

--- src/states/game/gui.gd
+++ src/states/game/gui.gd
@@ -21,13 +21,13 @@ func update_gem_display(gems_collected: int, total_gems: int = -1) -> void:
 
 
 func update_time_display(ticks: int) -> void:
-	@warning_ignore(integer_division)
+	@warning_ignore("integer_division")
 	var minutes := absi(ticks / (60 * Engine.physics_ticks_per_second))
 	var non_minute_ticks := ticks % (60 * Engine.physics_ticks_per_second)
-	@warning_ignore(integer_division)
+	@warning_ignore("integer_division")
 	var seconds := absi(non_minute_ticks / Engine.physics_ticks_per_second)
 	var non_second_ticks := ticks % Engine.physics_ticks_per_second
-	@warning_ignore(integer_division)
+	@warning_ignore("integer_division")
 	var milliseconds := absi(non_second_ticks * 1000 / Engine.physics_ticks_per_second)
 	var neg_sign := "" if ticks >= 0 else "-"
 	%TimeDisplay.text = "%s%02d:%02d.%03d" % [neg_sign, minutes, seconds, milliseconds]
